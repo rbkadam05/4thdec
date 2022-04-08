d59f85562955db4a1a95a196b33eea08715ae0e9
rbkadam05 <rbkadam05@gmail.com>
2022-04-08 11:06:53 +0530

    1st update

---
+++ src/constrctor/test.java
@@ -0,0 +1,11 @@
+package constrctor;
+
+public class test {
+	
+	    public static void main (String args[])   
+	    {  
+	        System.out.println(10 + 20 + "rahul");   
+	        System.out.println("rahul" + 10 + 20);  
+	    }  
+
+}
